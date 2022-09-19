4b5e4d2b195a44d43eff98273735786f2ca0f9c3
skiflok <anisimov.a.v@inbox.ru>
2022-09-19 10:36:53 +0300

    change readme and gitignore

---
+++ src/s21_matrix_oop.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Lok on 19.09.2022.
+//
+
+#include "s21_matrix_oop.h"
+
+namespace s21_matrix_oop {
+} // s21_matrix_oop
---
+++ src/s21_matrix_oop.h
@@ -0,0 +1,16 @@
+//
+// Created by Lok on 19.09.2022.
+//
+
+#ifndef S21_MATRIX_CPP_S21_MATRIX_OOP_H
+#define S21_MATRIX_CPP_S21_MATRIX_OOP_H
+
+namespace s21_matrix_oop {
+
+    class s21_matrix_oop {
+
+    };
+
+} // s21_matrix_oop
+
+#endif //S21_MATRIX_CPP_S21_MATRIX_OOP_H
